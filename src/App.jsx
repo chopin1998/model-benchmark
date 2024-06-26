610700dbe7ed4967ef241946b2299a491af63f4f
nomi30701 <neko30701@gmail.com>
2024-06-27 04:08:32 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,140 @@
+import './assets/App.css'
+import { useRef, useState } from 'react'
+import { inference } from './utils/inference'
+
+function App() {
+  const deviceRef = useRef(null);
+  const modelRef = useRef(null);
+  const warmUpFrequencyRef = useRef(null);
+  const testFrequencyRef = useRef(null);
+  const chartRef = useRef(null);
+  const startBenchmarkRef = useRef(null);
+  const benchmarkRef = useRef(null);
+  const [warmUpAvgTime, setWarmUpAvgTime] = useState('0');
+  const [testAvgTime, setTestAvgTime] = useState('0');
+
+  // setup chart
+  window.onload = () => {
+    const ctx = chartRef.current.getContext('2d');
+    benchmarkRef.current = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: [],
+        datasets: [{
+          label: 'Inference Time (ms)',
+          data: [],
+          borderColor: 'lightblue',
+          tension: 0.1
+        }]
+      },
+
+      // theme setting
+      options: {
+        plugins: {
+          legend: {
+            labels: {
+              font: {
+                size: 16
+              }
+            }
+          }
+        },
+        scales: {
+          x: {
+            ticks: {
+              font: {
+                size: 14
+              }
+            }
+          },
+          y: {
+            ticks: {
+              font: {
+                size: 14
+              }
+            }
+          }
+        }
+      },
+
+    });
+  }
+
+  // start benchmark button click
+  const startBenchmark = async () => {
+    startBenchmarkRef.current.disabled = true;
+    
+    // Reset chart data and labels and Reset Avg time
+    benchmarkRef.current.data.labels = [];
+    benchmarkRef.current.data.datasets[0].data = [];
+    setWarmUpAvgTime('0');
+    setTestAvgTime('0');
+
+    // config
+    const input_shape = [1, 3, 640, 640];
+    const model_path = `/${modelRef.current.value}.onnx`
+
+    await inference(
+      model_path, 
+      input_shape, 
+      deviceRef.current.value,
+      Number(warmUpFrequencyRef.current.value),
+      Number(testFrequencyRef.current.value),
+      benchmarkRef.current,
+      setWarmUpAvgTime,
+      setTestAvgTime
+    );
+
+    startBenchmarkRef.current.disabled = false;
+  }
+
+  return (
+    <>
+      <h1>Model Benchmark - onnxruntime Web</h1>
+
+      <div id='setting-container'>
+          <div>
+            <label htmlFor="device-selector">Device:</label>
+            <select name="device-selector" ref={deviceRef}>
+              <option value="webgpu">webGPU</option>
+              <option value="wasm">Wasm(cpu)</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="model-selector">Model:</label>
+            <select name="model-selector" ref={modelRef}>
+              <option value="yolov9-t-converted-simplify">yolov9-t</option>
+              <option value="yolov9-s-converted-simplify">yolov9-s</option>
+              <option value="gelan-s2">gelan-s2</option>
+              <option value="yolov10n">yolov10-n</option>
+              <option value="yolov10s">yolov10-s</option>
+            </select>
+            <p>Yolov10 only support wasm</p>
+          </div>
+
+          <div>
+            <label htmlFor="warmUpFrequency-input">Warm up frequency:</label>
+            <input type="number" id='warmUpFrequency-input' defaultValue={1} ref={warmUpFrequencyRef}/>
+          </div>
+          <div>
+            <label htmlFor="testFrequency-input">Testing frequency:</label>
+            <input type="number" id='testFrequency-input' defaultValue={100} ref={testFrequencyRef}/>
+          </div>
+      </div>
+
+      <button onClick={startBenchmark} ref={startBenchmarkRef}>Start benchmark</button>
+
+      <div id='info-container'>
+        <p>
+          Warm up Average Time: <span className='info-ms'>{warmUpAvgTime}ms</span>
+        </p>
+        <p>
+          Inference Average Time: <span className='info-ms'>{testAvgTime}ms</span>
+        </p> 
+      </div>
+      <canvas ref={chartRef}></canvas>
+    </>
+  )
+}
+
+export default App
---
+++ src/utils/inference.js
@@ -0,0 +1,67 @@
+import * as ort from "onnxruntime-web/webgpu"
+
+/**
+ * Inference function using ONNX model
+ * @param {String} model_path Yolo model path
+ * @param {Number[]} input_shape array of input shape
+ * @param {String} device device webgpu or cpu
+ * @param {Number} warmUpFrequency warm up frequency
+ * @param {Number} testFrequency test frequency
+ * @param {HTMLCanvasElement} benchmarkChart benchmark benchmarkChart
+ */
+export async function inference(
+    model_path,
+    input_shape,
+    device,
+    warmUpFrequency,
+    testFrequency,
+    benchmarkChart,
+    setWarmUpAvgTime,
+    setTestAvgTime
+) {
+    let totalWarmTime = 0;
+    let totalTestTime = 0; 
+
+    // load model
+    const onnxSession = await ort.InferenceSession.create(model_path, 
+        {executionProviders: [device]}
+    );
+    // if set wasm 
+    // ort.env.wasm.numThreads = n; // set number of threads
+
+    // Function to perform inference and measure execution time
+    async function performInference() {
+        const dummy_input_tensor = new ort.Tensor(
+            "float32",
+            new Float32Array(input_shape.reduce((a, b) => a * b)).map(() => Math.random()),
+            input_shape
+        );
+        const startTime = performance.now();
+        await onnxSession.run({ images: dummy_input_tensor });
+        const endTime = performance.now();
+        return endTime - startTime;
+    }
+
+    // Function to update the benchmark chart
+    function updateBenchmarkChart(iteration, executionTime) {
+        benchmarkChart.data.labels.push(iteration);
+        benchmarkChart.data.datasets[0].data.push(executionTime);
+        benchmarkChart.update();
+    }
+
+    // Warm up
+    for (let i = 0; i < warmUpFrequency; i++) {
+        const eps = await performInference();
+        totalWarmTime += eps;
+        updateBenchmarkChart(i, eps);
+        setWarmUpAvgTime((totalWarmTime / (i + 1)).toFixed(2));
+    }
+
+    // Test
+    for (let i = warmUpFrequency; i < testFrequency + warmUpFrequency; i++) {
+        const eps = await performInference();
+        totalTestTime += eps;
+        updateBenchmarkChart(i, eps);
+        setTestAvgTime((totalTestTime / (i - warmUpFrequency + 1)).toFixed(2));
+    }
+}
